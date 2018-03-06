b050a38632e3b81cf38b98679440461d95e053c7
AitorFont <aitor.font92@gmail.com>
2018-03-06 22:53:59 +0100

    Record/Save an audio file in music directory

--- app/src/main/java/aitor/font/puig/changeyourvoice/MainActivity.java
+++ app/src/main/java/aitor/font/puig/changeyourvoice/MainActivity.java
@@ -9,23 +9,19 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 
 import java.io.File;
 import java.io.IOException;
 
-import static android.Manifest.permission.RECORD_AUDIO;
-import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
-
 public class MainActivity extends AppCompatActivity {
-
-    public static final int RequestPermissionCode = 1;
 
     Boolean folderCreated = true, audioFileCreated = true;
 
     Button btn_record, btn_stop;
 
-    MediaRecorder myAudioRecorder = new MediaRecorder();
+    MediaRecorder audioRecorder;
     File audioFolder, audioFile;
 
     @Override
@@ -33,54 +29,69 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if(checkPermission()) {
-            audioFolder = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC) + "/ChangeYourVoice/");
-            if(!audioFolder.exists()) {
-                folderCreated = audioFolder.mkdir();
-            }
-
-            if(folderCreated) {
-                audioFile = new File(audioFolder.getAbsolutePath() + "/audioFile.mp3");
-                if(!audioFile.exists()) {
-                    try {
-                        audioFileCreated = audioFile.createNewFile();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
+        // Taking music folder reference
+        audioFolder = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC) + "/ChangeYourVoice/");
+        if(!audioFolder.exists()) {
+            folderCreated = audioFolder.mkdir();
         }
-        else {
-            requestPermission();
-        }
+
+        prepareAudioRecorder();
+        prepareInterface();
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
+    private void prepareAudioRecorder() {
+        audioRecorder = new MediaRecorder();
+        audioRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        audioRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        audioRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
     }
 
-    private void requestPermission() {
-        ActivityCompat.requestPermissions(MainActivity.this, new
-                String[]{WRITE_EXTERNAL_STORAGE, RECORD_AUDIO}, RequestPermissionCode);
-    }
+    private void prepareInterface() {
+        btn_record = findViewById(R.id.btn_record);
+        btn_stop = findViewById(R.id.btn_stop);
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
-
-    }
-
-    public boolean checkPermission() {
-        int result = ContextCompat.checkSelfPermission(getApplicationContext(), WRITE_EXTERNAL_STORAGE);
-        int result1 = ContextCompat.checkSelfPermission(getApplicationContext(), RECORD_AUDIO);
-        return result == PackageManager.PERMISSION_GRANTED && result1 == PackageManager.PERMISSION_GRANTED;
+        btn_record.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btn_record.setEnabled(false);
+                startRecording();
+                btn_stop.setEnabled(true);
+            }
+        });
+        btn_stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btn_stop.setEnabled(false);
+                stopRecording();
+                btn_record.setEnabled(true);
+            }
+        });
     }
 
     private void startRecording() {
+        if(folderCreated) {
+            audioFile = new File(audioFolder.getAbsolutePath() + "/audioFile.mp3");
+            if(!audioFile.exists()) {
+                try {
+                    audioFileCreated = audioFile.createNewFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
 
+        if(audioFileCreated) {
+            audioRecorder.setOutputFile(audioFile.getAbsolutePath());
+            try {
+                audioRecorder.prepare();
+                audioRecorder.start();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     private void stopRecording() {
-
+        audioRecorder.stop();
     }
 }
